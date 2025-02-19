ead852afa73757e81d82400332a4a379390d8f5e
Seth Barnard <sethsbarnard@gmail.com>
2025-02-19 15:01:11 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Seth Barnard on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
